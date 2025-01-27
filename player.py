91b4a403703fc4a9810906fd74f4886712788792
TheShadolo <theshadolo@gmail.com>
2025-01-27 14:43:58 -0500

    v8: added basic movement

--- player.py
+++ player.py
@@ -8,14 +8,23 @@ class Player(CircleShape):
     
     def rotate(self, dt): 
         self.rotation += (PLAYER_TURN_SPEED * dt)
+        print(f"rotation: {self.rotation}")
+    
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
     
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_a]:
+        if keys[pygame.K_a] and not keys[pygame.K_d]:
             self.rotate(-dt)
-        if keys[pygame.K_d]:
+        if keys[pygame.K_d] and not keys[pygame.K_a]:
             self.rotate(dt)
+        if keys[pygame.K_w] and not keys[pygame.K_s]:
+            self.move(dt)
+        if keys[pygame.K_s] and not keys[pygame.K_w]:
+            self.move(-dt)
     
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
